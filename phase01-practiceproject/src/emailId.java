9e61db2772923a23e0f5633e345e5ad0a5bf891d
mallika <mallikakakollu73954@gmail.com>
2022-07-20 22:04:42 +0530

    changes

---
+++ phase01-practiceproject/src/emailId.java
@@ -0,0 +1,26 @@
+import java.util.regex.*;    
+import java.util.*;    
+public class emailId{  
+    public static void main(String args[]){  
+        ArrayList<String> emails = new ArrayList<String>();  
+        emails.add("example@gmail.co.in");  
+        emails.add("abc@gmail.com");  
+        emails.add("harsha.name@gmail.com");  
+        emails.add("happy@domain.co.in");  
+        emails.add("xyz@gmail.com");  
+        emails.add("mallikakakollu@gmail.com");  
+       //invalid email  
+        emails.add("@yahoo.com");  
+        emails.add("javaTpoint#gmail.com");  
+        //Regular Expression   
+        String regex = "^(.+)@(.+)$";  
+        //Compile regular expression to get the pattern  
+        Pattern pattern = Pattern.compile(regex);  
+        //Iterate email array list  
+        for(String email : emails){  
+            //Create instance of matcher 
+        	Matcher matcher=pattern.matcher(email);
+            System.out.println(email +" : "+ matcher.matches()+"\n");  
+        }  
+    }  
+}
