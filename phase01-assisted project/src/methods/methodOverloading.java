298ada642dbe4399045df29f645305784768fc88
mallika <mallikakakollu73954@gmail.com>
2022-07-21 15:39:11 +0530

    assissted

---
+++ phase01-assisted project/src/methods/methodOverloading.java
@@ -0,0 +1,22 @@
+package methods;
+
+public class methodOverloading {
+	public void area(int b,int h)
+    {
+         System.out.println("Area of Triangle : "+(0.5*b*h));
+    }
+    public void area(int r) 
+    {
+         System.out.println("Area of Circle : "+(3.14*r*r));
+    }
+
+    public static void main(String args[])
+   {
+
+methodOverloading ob=new methodOverloading();
+       ob.area(10,12);
+       ob.area(5);  
+   }
+
+
+}
