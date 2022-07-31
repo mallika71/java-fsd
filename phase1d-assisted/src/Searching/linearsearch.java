cbcadce6caa62d038f0fd6d4a88e2c8c57ad3dc8
mallika <mallikakakollu73954@gmail.com>
2022-07-31 20:37:32 +0530

    assisted

--- phase1d-assisted/src/Searching/linearsearch.java
+++ phase1d-assisted/src/Searching/linearsearch.java
@@ -1,7 +1,7 @@
-package linearSearch;
+package Searching;
 
 import java.util.Scanner;
-public class linear {
+public class linearsearch45 {
 	
 	public static void main(String[] args) {
 		int arr[]={23,45,67,89,29,67,189,109};
